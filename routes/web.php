0cfe54a5a6e6ade58c815d6232c94697f57b0fe1
jeremyghst <51990006+jeremyghst@users.noreply.github.com>
2022-01-18 12:21:34 +0100

    authenticatie

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\TrackController;
 use App\Http\Controllers\RoomController;
@@ -17,22 +18,25 @@ use App\Http\Controllers\WorkstationController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('home');
+    });
 
-Route::get('/test', [TestController::class, 'getAll']);
-Route::get('/track', [TrackController::class, 'getAll']);
-Route::get('/tracks', [TrackController::class, 'getFirst']);
-Route::put('/tracks', [TrackController::class, 'setActive']);
-Route::post('/tracks', [TrackController::class, 'store']);
-Route::get('/room/{room}', [RoomController::class, 'getById']);
-Route::get('/rooms', [RoomController::class, 'getAll']);
-Route::post('/rooms', [RoomController::class, 'store']);
-Route::put('/rooms', [RoomController::class, 'update']);
-Route::delete('/room/{room}', [RoomController::class, 'destroy']);
-Route::post('/workstation', [WorkstationController::class, 'store']);
-Route::put('/workstation', [WorkstationController::class, 'update']);
-Route::put('/ws', [WorkstationController::class, 'updateJSON']);
-Route::delete('/workstation/{workstation}', [WorkstationController::class, 'destroy']);
-Route::get('/track/{track}', [TrackController::class, 'getById']);
+    Route::get('/test', [TestController::class, 'getAll']);
+    Route::get('/track', [TrackController::class, 'getAll']);
+    Route::get('/tracks', [TrackController::class, 'getFirst'])->name('home');
+    Route::put('/tracks', [TrackController::class, 'setActive']);
+    Route::post('/tracks', [TrackController::class, 'store']);
+    Route::get('/room/{room}', [RoomController::class, 'getById']);
+    Route::get('/rooms', [RoomController::class, 'getAll']);
+    Route::post('/rooms', [RoomController::class, 'store']);
+    Route::put('/rooms', [RoomController::class, 'update']);
+    Route::delete('/room/{room}', [RoomController::class, 'destroy']);
+    Route::post('/workstation', [WorkstationController::class, 'store']);
+    Route::put('/workstation', [WorkstationController::class, 'update']);
+    Route::put('/ws', [WorkstationController::class, 'updateJSON']);
+    Route::delete('/workstation/{workstation}', [WorkstationController::class, 'destroy']);
+    Route::get('/track/{track}', [TrackController::class, 'getById']);
+});
